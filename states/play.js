e53e641da3dcbf3330dfe8dd561aafee12a5e1f3
Mission Bit <missionbit@35a-MissionBit.local>
2018-07-23 09:48:41 -0700

    new door

--- states/play.js
+++ states/play.js
@@ -11,17 +11,18 @@ const worldWidth = 1600;
 const worldHeight = 600;
 const startHealth = 3;
 const maxHealth = 5; 
+let door;
 
 export default class Play {
     preload() {
         this.load.image('sky', 'src/games/firstgame/assets/sky.png');
         this.load.image('road', 'src/games/firstgame/assets/road.png');
         this.load.image('star', 'src/games/firstgame/assets/star.png');
-        this.load.spritesheet('dude', 'src/games/firstgame/assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+        this.load.spritesheet('dude', 'src/games/firstgame/assets/dude.png', { frameWidth: 32, frameHeight: 48 }); 
         this.load.image('heart', 'src/games/firstgame/assets/heart.png');
-        this.load.image('house1','src/games/firstgame/assets/house1.png');
-        this.load.image('house2','src/games/firstgame/assets/house2.png');
-        
+        this.load.image('house1', 'src/games/firstgame/assets/house1.png');
+        this.load.image('house2', 'src/games/firstgame/assets/house2.png');
+        this.load.spritesheet('door1', 'src/games/firstgame/assets/door1.png', { frameWidth:150, frameHeight: 150});
     }
     init() {
         this.score = 0;
@@ -49,8 +50,17 @@ export default class Play {
         this.add.image(800, 300, 'sky');
         this.add.image(1200, 300, 'sky');
         this.add.image(350, 374, 'house1');
-        const house2=this.add.image(900, 314, 'house2');
+        const house2 = this.add.image(900, 314, 'house2');
         house2.setScale(1.78)
+
+
+        door = this.add.sprite(1350, 524, 'door1');
+        this.anims.create({
+            key: 'door-animation',
+            frames: this.anims.generateFrameNumbers('door1', { start: 0, end: 2 }),
+            frameRate: 10,
+            repeat: -1
+        });
 
         road = this.physics.add.staticGroup();
 
@@ -61,8 +71,7 @@ export default class Play {
         road.create(950, 799, 'road').setScale(2).refreshBody();
         road.create(1150, 799, 'road').setScale(2).refreshBody();
         road.create(1350, 799, 'road').setScale(2).refreshBody();
-        
-    
+            
         player = this.physics.add.sprite(100, 450, 'dude');
         player.setBounce(0.2);
         player.setCollideWorldBounds(true);
@@ -91,14 +100,11 @@ export default class Play {
             repeat: -1
         });
 
-        
-
-
-        stars = this.physics.add.group ();
+        stars = this.physics.add.group();
         stars.defaultKey = "star";
-        stars.create (1300, 100);
-        stars.children.iterate (child => {
-            child.setBounceY(Phaser.Math.FloatBetween (0.1,0.3));
+        stars.create(1300, 100);
+        stars.children.iterate(child => {
+            child.setBounceY(Phaser.Math.FloatBetween(0.1, 0.3));
         });
 
         stars = this.physics.add.group();
@@ -120,7 +126,9 @@ export default class Play {
 
     }
 
-    update() {        
+    update() {       
+        door.anims.play('door-animation') 
+
         if (gameOver) {
             return;
         }
@@ -144,11 +152,10 @@ export default class Play {
     }
     collectStar(player, star) {
         star.disableBody(true, true);
-        this.score += 10;
         this.events.emit("updateHUD");
     }
     collectHeart(player, heart) {
-        heart.disableBody(true,true);
+        heart.disableBody(true, true);
         this.health += 1; 
     }
 }
